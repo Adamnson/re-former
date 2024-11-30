e602ac2d1d6b0204254aaeaf1c8a69dc09a06c91
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-11-30 21:23:33 +0530

    (feat) add error message display in forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
