c3148e2a6dca45f6f260f701f7e07034a57b2568
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-11-30 21:14:09 +0530

    (feat) added show and edit in controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(allowed_user_params)
